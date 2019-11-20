0c33cbd32e9a5a1cbf6af551f15f3a63b8c8f8ce
D1M1MAGOO <dimimagoo2015@gmail.com>
2019-11-20 13:20:29 +1100

    Tracker update and history

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -7,36 +7,7 @@ const routes: Routes = [
     path: 'tabs',
     component: TabsPage,
     children: [
-      {
-        path: 'tab1',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab1/tab1.module').then(m => m.Tab1PageModule)
-          }
-        ]
-      },
-      {
-        path: 'tab2',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab2/tab2.module').then(m => m.Tab2PageModule)
-          }
-        ]
-      },
-      {
-        path: 'tab3',
-        children: [
-          {
-            path: '',
-            loadChildren: () =>
-              import('../tab3/tab3.module').then(m => m.Tab3PageModule)
-          }
-        ]
-      },
+      
       {
         path: 'tracker',
         children: [
@@ -61,14 +32,14 @@ const routes: Routes = [
 
       {
         path: '',
-        redirectTo: '/tracker/tracker',
+        redirectTo: 'tracker',
         pathMatch: 'full'
       }
     ]
   },
   {
     path: '',
-    redirectTo: '/tracker/tracker',
+    redirectTo: 'tabs/tracker',
     pathMatch: 'full'
   }
 ];
--- src/app/tracker/tracker.page.ts
+++ src/app/tracker/tracker.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder ,FormGroup }from '@angular/forms';
+import {timer, Subscription, VirtualTimeScheduler } from 'rxjs';
+
+import { DataService } from '../data.service';
+import {Task} from '../models/task.interface'
 
 @Component({
   selector: 'app-tracker',
@@ -7,20 +12,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TrackerPage implements OnInit {
 
+  taskForm:FormGroup;
   started:Boolean = false;
+  startTime:number;
+  stopTime: number;
 
-  constructor() { }
+  timerSub: Subscription;
+  time: number;
+
+  constructor(
+    private formBuilder:FormBuilder,
+    private dataService :DataService
+  ) { }
 
   ngOnInit() {
+    this.taskForm = this.formBuilder.group({
+      name: ['', [ Validators.required, Validators.minLength(3) ] ]
+    });
   }
 
   start(){
     this.started = true;
-
+    this.startTime = new Date().getTime();
+    const tm = timer(0,1000);
+    this.timerSub = tm.subscribe(val => this.time = val);
+    console.log( this.startTime);
   }
 
   stop(){
     this.started = false;
+    this.stopTime = new Date().getTime();
+    this.timerSub.unsubscribe();
+    this.save();
+    console.log( this.stopTime);
 
+  }
+
+  reset(){
+    this.stopTime = new Date().getTime();
+    this.timerSub.unsubscribe();
+    console.log( this.stopTime);
+
+    this.startTime = new Date().getTime();
+    const tm = timer(0,1000);
+    this.timerSub = tm.subscribe(val => this.time = val);
+    console.log( this.startTime);
+  }
+
+  save(){
+    let task:Task = {
+      name: this.taskForm.get('name').value,
+      start: this.startTime, 
+      stop: this.stopTime
+    }
+    this.dataService.addToList( task);
   }
 }
